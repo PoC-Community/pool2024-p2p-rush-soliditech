7a7669f18ea86b6de5a8f86518c0ed58861ff5e1
Lucas Leclerc <lucas.leclerc@epitech.eu>
2024-02-03 22:09:38 +0100

    [ADD] add nft with metadata

--- nftek/src/nft.sol
+++ nftek/src/nft.sol
@@ -2,20 +2,29 @@
 pragma solidity ^0.8.20;
 
 import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {IERC721Metadata} from "../lib/openzeppelin-contracts/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import {Strings} from "../lib/openzeppelin-contracts/contracts/utils/Strings.sol";
+import {IERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+import {IERC165} from "../lib/openzeppelin-contracts/contracts/utils/introspection/IERC165.sol";
+import {IERC721Metadata} from "../lib/openzeppelin-contracts/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 
-contract NFTek is ERC721, Ownable {
+contract NFTek is IERC721Metadata, ERC721, Ownable {
     string internal baseURI;
     uint256 internal MAXNFT;
     uint256 internal nbNft;
     uint256 public price;
     bool public saleState;
+    string public nftName;
+    string public nftSymbole;
 
-    constructor(string memory _startingBaseURI, uint256 _maxnft, uint256 _price) ERC721("NFTek", "NTEK") Ownable(msg.sender) {
+    constructor(string memory _name, string memory _sym, string memory _startingBaseURI, uint256 _maxnft, uint256 _price) ERC721(_name, _sym) Ownable(msg.sender) {
         baseURI = _startingBaseURI;
         MAXNFT = _maxnft;
         price = _price;
         saleState = true;
+        nftName = _name;
+        nftSymbole = _sym;
     }
 
     function changeMaxNft(uint256 max) public onlyOwner {
@@ -54,5 +63,22 @@ contract NFTek is ERC721, Ownable {
     }
     function getNbNFTek() public view returns (uint256) {
         return nbNft;
+    }
+    function tokenURI(uint256 tokenId) public view override(ERC721, IERC721Metadata) returns (string memory) {
+        require(tokenId <= nbNft, "wrong tokenId");
+        return string(abi.encodePacked(baseURI, Strings.toString(tokenId)));
+    }
+    function name() public view override(ERC721, IERC721Metadata) returns (string memory) {
+        return nftName;
+    }
+    function symbol() public view override(ERC721, IERC721Metadata) returns (string memory) {
+        return nftSymbole;
+    }
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, IERC165) returns (bool) {
+        return
+            interfaceId == type(IERC721).interfaceId ||
+            interfaceId == type(IERC721Metadata).interfaceId ||
+            interfaceId == bytes4(0x49064906) ||
+            super.supportsInterface(interfaceId);
     }
 }
